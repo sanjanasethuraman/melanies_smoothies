449bed4cfb77dc556b36f903f5e34fcd52e61687
Sanjana Sethuraman <69803757+sanjanasethuraman@users.noreply.github.com>
2024-03-11 10:52:15 +0530

    Update streamlit_app.py
    Added fruityvice api data to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,8 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success(f'Your Smoothie is ordered, {name_on_order}', icon="✅")
+
+#new section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
