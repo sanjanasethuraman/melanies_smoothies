4077053bab7ee9023cfa7f82198e71e62c521e06
Sanjana Sethuraman <69803757+sanjanasethuraman@users.noreply.github.com>
2024-03-11 12:57:09 +0530

    Update streamlit_app.py
    Added health facts about fruits in the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,5 @@ if time_to_insert:
 #new section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
